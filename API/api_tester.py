1d421beaf61bce54f0f0ac50400355eb1a82d7c6
NtemKenyor <nkenyor@gmail.com>
2024-08-20 19:15:08 +0100

    on it

--- API/api_tester.py
+++ API/api_tester.py
@@ -1,6 +1,8 @@
 import requests
 
-url = "http://127.0.0.1:5000/create-video"
+# url = "http://127.0.0.1:5000/create-video"
+url_api = "https://roynek.com/AI-Viddor/API"
+url = "https://roynek.com/AI-Viddor/API/create-video"
 
 data = {
     "images": [
